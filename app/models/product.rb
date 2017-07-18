e537f609429b0bb2f97673a14ffb370ae99dbaef
Phil Kim <phil.kim@shopify.com>
2017-07-18 10:31:40 -0400

    Task C - Catalog display

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,10 @@
 class Product < ApplicationRecord
 
    validates :title, :description, :image_url, presence: true
+   validates :title, length: {
+     minimum: 10,
+     message: "The title should be at least 10 characters long"
+   }
    validates :price, numericality: {greater_than_or_equal_to: 0.01}
    validates :title, uniqueness: true
    validates :image_url, allow_blank: true, format: {
