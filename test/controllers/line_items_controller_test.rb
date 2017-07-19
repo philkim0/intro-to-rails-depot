75531e8d56d96aa6a30301c7dd5a92f2f2ff89ca
Phil Kim <phil.kim@shopify.com>
2017-07-19 13:12:20 -0400

    Task E, before playtime

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -22,7 +22,7 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
 
     follow_redirect!
 
-    assert_select 'h2', 'Your Pragmatic Cart'
+    assert_select 'h2', 'Your Cart'
     assert_select 'li', 'Ruby Performance Optimization'
   end
 
@@ -37,7 +37,8 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update line_item" do
-    patch line_item_url(@line_item), params: { line_item: { cart_id: @line_item.cart_id, product_id: @line_item.product_id } }
+    patch line_item_url(@line_item),
+    params: { line_item: { product_id: @line_item.product_id } }
     assert_redirected_to line_item_url(@line_item)
   end
 
