1fd3b633f3a7962930ce4d4e37e54b22c1423588
Phil Kim <phil.kim@shopify.com>
2017-07-21 11:39:48 -0400

    Mailer added - smtp, Tests for mailers written

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,3 +52,17 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
+
+Depot::Application.configure do
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address:             "smtp.gmail.com",
+    port:                587,
+    domain:              "domain.of.sender.net",
+    authentication:      "plain",
+    user_name:           "dave",
+    password:            "secret",
+    enable_starttls_auto: true
+  }
+end
